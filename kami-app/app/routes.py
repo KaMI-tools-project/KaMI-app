f5b700f7b4823ddd45dd6e26e56006af3b7fb465
Alix Chagué <alix.chague@inria.fr>
2021-09-30 15:44:39 +0200

    creation des dataframes dans routes manque dans template

--- kami-app/app/routes.py
+++ kami-app/app/routes.py
@@ -1,17 +1,36 @@
+import csv
+from datetime import datetime
 import glob
 import os
 import random
-from typing_extensions import Required
 
 from flask import render_template, request, send_file, send_from_directory, url_for, redirect, flash
-from wtforms import Form, BooleanField, StringField, validators
+from kami.Kami import Kami
+import pandas as pd
 from werkzeug.utils import secure_filename
 
 from .app import app
-#from .aspyrelib import aspyre
 
-from kami.Kami import Kami
 
+
+"""
+#TODO: implémenter un bouton d'export CSV
+# tiré du colab de KaMI
+def make_csv():
+    name_csv = f"evaluation_report_kami_{metadatas['DATETIME']}_{metadatas['MODEL']}.csv"
+    with open(name_csv, 'w')  as csv_file:
+        writer = csv.writer(csv_file, 
+                            delimiter=',',
+                            quotechar='|', 
+                            quoting=csv.QUOTE_MINIMAL)
+    for key, value in metadatas.items():
+        row = []
+        row.append(key)
+        row.append(value)
+        writer.writerow(row)
+        df_metrics.to_csv(name_csv, mode='a', header=True)
+        files.download(name_csv)
+"""
 
 class kamiForm():
     def validate(self, form):
@@ -35,12 +54,39 @@ class kamiForm():
             self.options = "".join(self.options)
 
 
+def make_dataframe(score_board, reference):
+    metadata_keys = ['levensthein_distance_char', 'levensthein_distance_words', 'hamming_distance',  'wer',  'cer',  'wacc',  'mer',  'cil',  'cip',  'hits',  'substitutions',  'deletions',  'insertions']
+    now = datetime.now()
+    metadatas = {}
+    metrics = {}
+    metadatas["DATETIME"] = now.strftime("%d_%m_%Y_%H:%M:%S")
+    metadatas["IMAGE"] = None  #TODO changer quand implémenté
+    metadatas["REFERENCE"] = reference
+    metadatas["MODEL"] = None #TODO changer quand implémenté
+
+    for key, value in score_board.items():
+        if type(value) != dict and key not in metadata_keys:
+            metadatas[key] = value
+        else:
+            metrics[key] = value
+    try:
+        df_metrics = pd.DataFrame.from_dict(metrics)
+    except:
+        df_metrics = pd.DataFrame.from_dict(metrics, orient='index')
+    
+    tables=[df_metrics.to_html(classes="data")]
+    titles=[df.columns.values]
+    return tables, titles
+
+    
+
 # ROUTES ##############################################################################################################
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Generate the index page and handles comparing 2 strings with KaMI"""
     error = None
-    score_board = None
+    tables = None
+    titles = None
 
     if request.method == "POST":
         kami_form = kamiForm(request.form)
@@ -52,7 +98,7 @@ def index():
                 percent=app.config["KAMI_OPT_PERC"],
                 round_digits=app.config["KAMI_OPT_ROUND"],
                 apply_transforms=kami_form.options)
-            score_board = kevaluator.scores.board
+            tables, titles = make_dataframe(kevaluator.scores.board)
         else:
             error = "Invalid Form"
-    return render_template('page/index.html', title="KaMI App | Upload", kami_version = app.config['KAMI_VERSION'], error=error, score_board=score_board)
+    return render_template('page/index.html', title="KaMI App | Upload", kami_version = app.config['KAMI_VERSION'], error=error, tables=tables, titles=titles)
