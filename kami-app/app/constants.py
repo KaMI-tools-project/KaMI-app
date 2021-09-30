3820103a1af7b72fd396b0e6e733e62399e9a049
Alix Chagué <alix.chague@inria.fr>
2021-09-30 12:55:52 +0200

    modif template

--- kami-app/app/constants.py
+++ kami-app/app/constants.py
@@ -1,7 +1,9 @@
 import os
+import kami
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'uploads')
-ALLOWED_EXTENSIONS = set(['zip'])
+ALLOWED_EXTENSIONS = set(['txt'])
 MAX_FILESIZE = 500000000 #  Allowing 500 MB max
 
-KAMI_VERSION = "0.3"
+#TODO add KaMI version
+KAMI_VERSION = kami.__version__
