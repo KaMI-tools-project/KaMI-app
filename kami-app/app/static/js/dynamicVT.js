dcd2f0c3a2a4f15952d9fd385035036c9f5bd3f8
Lucaterre <ls.terriel@gmail.com>
2022-04-28 18:11:51 +0200

    light refactor in dynamicVT

--- kami-app/app/static/js/dynamicVT.js
+++ kami-app/app/static/js/dynamicVT.js
@@ -1,12 +1,7 @@
 $('.line').hover(function () {
-    //console.log($(this).data('id'))
     var $t = $(this);
     var $i = $('#' + $t.data('id'));
     let type = $t.attr('class').split(' ')[0];
-
-    //var type = $t.data('id').split('-')[0];
-
-    // find offset positions for the word (t = this) and image (i)
     var ot = {
         x: $t.offset().left + $t.width() / 2,
         y: $t.offset().top + $t.height() / 2
@@ -24,8 +19,6 @@ $('.line').hover(function () {
         y: ot.y < oi.y ? ot.y : oi.y,
         y1: ot.y > oi.y ? ot.y : oi.y
     };
-
-
     // create canvas between those points
     var c = $('<canvas/>').attr({
         'width': p.x1 - p.x + 1,
@@ -39,7 +32,6 @@ $('.line').hover(function () {
     }).appendTo($('body'))[0].getContext('2d');
 
     // draw line
-
     if (type === "insertion"){
         color = "#4169E1"
     }if (type === "delSubts"){
@@ -57,7 +49,6 @@ $('.line').hover(function () {
     c.stroke();
     $($i).addClass('char')
     $($t).addClass('char')
-    //drawDot(c, ot.x - p.x, ot.y - p.y)
 
 }, function () {
     $('canvas').remove();
